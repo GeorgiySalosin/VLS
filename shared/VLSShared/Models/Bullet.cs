e1fa49869c1de967d61aed0386110440a8157436
bobr <bobr@DESKTOP-K891U1S>
2026-04-01 20:33:08 +0700

    Add fields

--- shared/VLSShared/Models/Bullet.cs
+++ shared/VLSShared/Models/Bullet.cs
@@ -1,4 +1,6 @@
-﻿namespace VLSShared.Models
+﻿using VLSGame.Models;
+
+namespace VLSShared.Models
 {
     internal class Bullet
     {
@@ -7,5 +9,31 @@
         private const double G1 = 0.445; // Баллистический коэффицент
         private const double P = 1.225; // Плотность воздуха, кг/м3
         private const double D = 0.00792; // Диаметр пули, м
+
+        // todo: Разобраться, какие константы нужны
+        private const double Cd = 0.674; // todo: Коэффициент лобового сопротивления
+        private const double S = Math.PI * (D / 2 * (D / 2)); // Площадь поперечного сечения пули, м2
+        private const double F_air = 0.5 * P * V * V * Cd * S; // Сила сопротивления воздуха
+        private const double G = 9.81; // Ускорение свободного падения, м/с2
+        private const double I = M * 2.2 / ((D * 39.37) * (D * 39.37) * G1); // Формула форм-фактора + перевод кг в фунты, м в дюймы
+
+        private readonly int X;
+        private int Y;
+        private double Distance = 0;
+        private readonly PanoramaData Panorama;
+
+        internal Bullet(int x, int y, PanoramaData panorama)
+        {
+            X = x;
+            Y = y;
+            Panorama = panorama;
+        }
+
+        internal void Process()
+        {
+            if (Panorama == null) return;
+            if (Panorama.GetDistanceAtPixel(X, Y) <= Distance) return;
+            // todo
+        }
     }
 }
