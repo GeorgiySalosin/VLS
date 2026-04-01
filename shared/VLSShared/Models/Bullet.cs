0628bfa1387476223dd1b87337d744f2908abb27
bobr <bobr@DESKTOP-K891U1S>
2026-04-01 16:47:02 +0700

    Create class Bullet with consts

---
+++ shared/VLSShared/Models/Bullet.cs
@@ -0,0 +1,11 @@
+﻿namespace VLSShared.Models
+{
+    internal class Bullet
+    {
+        private const double M = 0.0113; // Масса, кг
+        private const int V = 800; // Начальная скорость, м/сек
+        private const double G1 = 0.445; // Баллистический коэффицент
+        private const double P = 1.225; // Плотность воздуха, кг/м3
+        private const double D = 0.00792; // Диаметр пули, м
+    }
+}
