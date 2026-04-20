0713c933acf79527714d58a79f786eb9f3a9a51b
bobr <bobr@DESKTOP-K891U1S>
2026-04-20 17:44:01 +0700

    Added RelayCommand, binded ToggleModeGridCommand to button

---
+++ vlsgame/Commands/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace VLSGame.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        internal RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(Execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) { if (CanExecute(parameter)) execute(parameter); }
+    }
+}
--- vlsgame/ViewModels/LobbyViewModel.cs
+++ vlsgame/ViewModels/LobbyViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using VLSGame.Commands;
 using VLSGame.Models;
 
 namespace VLSGame.ViewModels
@@ -6,6 +9,28 @@ namespace VLSGame.ViewModels
     internal class LobbyViewModel : ViewModelBase
     {
         public ObservableCollection<MapButtonData> Maps { get; set; }
+
+        private Visibility visibilityGridMode = Visibility.Hidden;
+        public Visibility VisibilityGridMode {
+            get => visibilityGridMode;
+            private set => Set(ref visibilityGridMode, value);
+        }
+
+        #region Commands
+
+        #region ChangeVisibilityModeGrid
+
+        public ICommand ToggleModeGridCommand { get; }
+        private bool CanToggleModeGridCommandExecute(object p) => true;
+        private void OnToggleModeGridCommandExecuted(object p)
+        {
+            if (VisibilityGridMode == Visibility.Hidden) VisibilityGridMode = Visibility.Visible;
+            else VisibilityGridMode = Visibility.Hidden;
+        }
+
+        #endregion
+
+        #endregion
 
         internal LobbyViewModel()
         {
@@ -15,21 +40,23 @@ namespace VLSGame.ViewModels
                 {
                     Title = "Sunny",
                     Subtitle = "common map",
-                    MapBackgroundImage = "/Content/Maps/Test_W.png"
+                    MapBackgroundImage = "/Content/Lobby/T_MapPreview_Sun.png"
                 },
                 new MapButtonData
                 {
                     Title = "Overcast",
                     Subtitle = "for nature lovers",
-                    MapBackgroundImage = "/Content/Maps/Test_W.png"
+                    MapBackgroundImage = "/Content/Lobby/T_MapPreview_Fog.png"
                 },
                 new MapButtonData
                 {
                     Title = "Dark",
                     Subtitle = "prove your skill!",
-                    MapBackgroundImage = "/Content/Maps/Test_W.png"
+                    MapBackgroundImage = "/Content/Lobby/T_MapPreview_Sunset.png"
                 }
             };
+
+            ToggleModeGridCommand = new RelayCommand(OnToggleModeGridCommandExecuted, CanToggleModeGridCommandExecute);
         }
     }
 }
