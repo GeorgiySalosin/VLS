fba8372cd689a5e099175bcaf6078063e16177cf
dobrovolskiyiv <dobrovolskiyiv@209D-8>
2026-04-21 15:26:18 +0700

    Start button in LobbyView works, but LobbyView isn't closed and dirty code in LobbyViewModel

--- vlsgame/ViewModels/LobbyViewModel.cs
+++ vlsgame/ViewModels/LobbyViewModel.cs
@@ -3,6 +3,9 @@ using System.Windows;
 using System.Windows.Input;
 using VLSGame.Commands;
 using VLSGame.Models;
+using VLSGame.Services;
+using VLSGame.Views;
+using VLSShared.Interfaces;
 
 namespace VLSGame.ViewModels
 {
@@ -26,6 +29,61 @@ namespace VLSGame.ViewModels
         {
             if (VisibilityGridMode == Visibility.Hidden) VisibilityGridMode = Visibility.Visible;
             else VisibilityGridMode = Visibility.Hidden;
+        }
+
+        #region SinglePlayer
+
+        private IGameMode? currentGameMode;
+        public IGameMode? CurrentGameMode => currentGameMode;
+        private async Task StartSinglePlayerAsync(string panoramaPath)
+        {
+            currentGameMode = new SinglePlayerGameMode();
+            await currentGameMode.StartAsync();
+
+            if (currentGameMode is SinglePlayerGameMode singlePlayer)
+            {
+                singlePlayer.SetPanoramaPath(panoramaPath);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region StartGame
+
+        public ICommand StartGame { get; }
+        private bool CanStartGameExecute(object p) => true; // todo
+        private async void OnStartGameExecuted(object p)
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // currently we only use the test panorama
+
+            string ColorMapPath = System.IO.Path.Combine(basePath, @"Content\Maps\Test_W.png");
+            string DepthMapPath = System.IO.Path.Combine(basePath, @"Content\Maps\Test_D.png");
+
+            if (!System.IO.File.Exists(ColorMapPath))
+            {
+                MessageBox.Show($"Color map was not found from {ColorMapPath}", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!System.IO.File.Exists(DepthMapPath))
+            {
+                MessageBox.Show($"Depth map was not found from {DepthMapPath}", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            await StartSinglePlayerAsync(ColorMapPath);
+
+            // Открываем окно Match с обоими путями
+            var matchViewModel = new MatchViewModel(CurrentGameMode!, ColorMapPath, DepthMapPath);
+            var matchWindow = new Match(matchViewModel);
+            matchWindow.Show();
+            //this.Close(); todo
         }
 
         #endregion
@@ -57,6 +115,7 @@ namespace VLSGame.ViewModels
             };
 
             ToggleModeGridCommand = new RelayCommand(OnToggleModeGridCommandExecuted, CanToggleModeGridCommandExecute);
+            StartGame = new RelayCommand(OnStartGameExecuted, CanStartGameExecute);
         }
     }
 }
