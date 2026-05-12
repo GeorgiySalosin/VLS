71a9a6930d4e10e7a028a2553772537220a58c88
salosings <gsalosin@gmail.com>
2026-05-12 17:10:02 +0700

    add commento

--- shared/VLSShared/Models/HitZoneInfo.cs
+++ shared/VLSShared/Models/HitZoneInfo.cs
@@ -9,10 +9,18 @@ namespace VLSShared.Models
 {
     public sealed record HitZoneInfo
     {
+        /// <summary>
+        /// Base damage applied to enemy in case of hit
+        /// </summary>
         public int BaseDamage { get; init; }
+        /// <summary>
+        /// A scale of blood texture that appears on enemy hit
+        /// </summary>
         public double FXScale { get; init; }
 
-        
+        /// <summary>
+        /// An actual representation of zone where we hit
+        /// </summary>
         private HitZone HitZone { get; init; }
 
         private HitZoneInfo(HitZone hitZone, int baseDamage, double fxScale)
@@ -22,7 +30,7 @@ namespace VLSShared.Models
             FXScale = fxScale;
         }
 
-        // Static instances
+        // Static pre-created instances
         public static readonly HitZoneInfo None = new(HitZone.None, 0, 0.0);
         public static readonly HitZoneInfo Head = new(HitZone.Head, 105, 7.0);
         public static readonly HitZoneInfo Body = new(HitZone.Body, 60, 5.5);
